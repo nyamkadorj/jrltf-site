7d0da9fa247686d219102f85f7856528e5a6e80b
Nyamdemberel Dorj <nyamdembereldorj@Nyamdemberels-MacBook-Air.local>
2026-02-01 20:49:02 +0900

    Update home and framework pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export const metadata: Metadata = {
-  title: "Jr.LTF",
+  title: "Junior.LTF",
   description: "Junior Leadership Task Force – Program & Recruitment",
 };
 
